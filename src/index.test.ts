e8596173678368fe9f1dd25127b3e7e5d72b86a0
Deftunk <martin.lavie.pro@gmail.com>
2020-01-07 12:40:15 +0100

    [IMPLEMENTATION] first test passed

--- src/index.test.ts
+++ src/index.test.ts
@@ -7,9 +7,17 @@ describe('Oh ! Looks like there is a new project here', () => {
         })
     })
 
-    describe('let\'s find those numbers ', () => {
-        it('it should return 0', () => {
+    describe('let\'s find those numbers ', () => {        
+        it('should return 0 if we try with 0', () => {
             expect(findClosestToZero([0])).toBe(0);
+        })
+
+        it('should return 0 if we try with []', () => {
+            expect(findClosestToZero([])).toBe(0);
+        })
+
+        it('should return 5 for [8, 5, 10]', () => {
+            expect(findClosestToZero([8, 5, 10])).toBe(5);
         })
     })
 });
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
 const findClosestToZero = (numbers: number[]) => {
-    return numbers[0];
+
+    //undefined or any false args are impossible because of ts so we only test 0    
+    const closestToZero : number = numbers.reduce((actualClosest: number, maybeTheClosest: number) => {        
+        if (Math.abs(maybeTheClosest) < Math.abs(actualClosest) || Math.abs(actualClosest) === 0) {
+            return Math.abs(maybeTheClosest);
+        }
+        else return actualClosest
+    }, 0);
+    return closestToZero;
 }
 
 export { findClosestToZero };
