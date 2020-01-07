20e83290cc9e8dd7a6b8c1c011b8019c711bf4fd
Deftunk <martin.lavie.pro@gmail.com>
2020-01-07 13:51:15 +0100

    [CORRECTION] typo

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 const findClosestToZero = (numbers: number[]): number => {
     // Undefined or any false args are impossible because of ts so we only test 0
-    // This reduce can be shorter but for readability is important
+    // This reduce can be shorter but readability is important
     return numbers.reduce((actualClosest: number, maybeTheClosest: number) => {
         return closestFromZero(maybeTheClosest, actualClosest) ? maybeTheClosest : actualClosest
     }, 0);
@@ -8,7 +8,6 @@ const findClosestToZero = (numbers: number[]): number => {
 
 const closestFromZero = (number1: number, number2: number): boolean => {
     return Math.abs(number1) < Math.abs(number2) || Math.abs(number2) === 0
-    
 }
 
 export { findClosestToZero };
